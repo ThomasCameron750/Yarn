c31b8b257f8cbad27ba04ca977e8fa3cd0341e08
Thomas Cameron <cameronts7@gmail.com>
2021-12-06 22:38:18 -0500

    Disabled raw line output, additional regex, colorized output

--- yarn.py
+++ yarn.py
@@ -1,10 +1,23 @@
 import subprocess
 import sys
 import re
+import os
+
+def printColor(prefix, line):
+	if os.name == "nt":
+		os.system('color')
+	print ('\033[93m' + prefix + '\033[0m' + line)
+
+cryptocurrencies = ["Bitcoin", "Litecoin", "Namecoin", "Peercoin", "Dogecoin", "Gridcoin", "Primecoin", "Auroracoin", "Dash", "MazaCoin", "Monero", "Titcoin", "Verge", "Stellar", "Vertcoin", "Ethereum", "Ethereum Classic", "Nano", "Tether", "Firo", "Zcash", "Bitcoin Cash", "EOS.IO", "Cardano", "Tron", "AmbaCoin", "Algorand", "Shiba Inu", "BitClout", "SafeMoon", "Coinye", "OneCoin", "BitConnect", "KodakCoin", "Petro"]
+
+network_terms = ["connection", "socket", " port ", "address", "tcp", "udp", "voip", " ping ", "traceroute", "tracert"]
+
+file_extensions = [".BAT", ".BIN", ".CMD", ".COM", ".CPL", ".EXE", ".INF", ".INS", ".INX", ".ISU", ".JOB", ".JSE", ".LNK", ".MSC", ".MSI", ".MSP", ".MST", ".PAF", ".PIF", ".PS1", ".REG", ".RGS", ".SCR", ".SCT", ".SHB", ".SHS", ".U3P", ".VB", ".VBE", ".VBS", ".VBSC,RIPT" ".WS", ".WSF", ".WSH", ".JAR", ".DLL", ".PDB"]
 
 if len(sys.argv) is not 2:
 	print("Usage: yarn.py <name of file>")
 	exit()
+
 
 command = "strings " + sys.argv[1]
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
@@ -12,38 +25,44 @@ output, errors = process.communicate()
 
 # probably important things 
 output = output.decode()
-print("Any string starting with 2 letters, length of 5+\n")
-for line in output.split("\n"):
-	if re.search(r"^[A-Za-z]{2}.*", line):
-		if len(line) > 5:
-			print(line)
+# print("Any string starting with 2 letters, length of 5+\n")
+# for line in output.split("\n"):
+# 	if re.search(r"^[A-Za-z]{2}.*", line):
+# 		if len(line) > 5:
+# 			print(line)
 
 # probably important things 
 for line in output.split("\n"):
-	if re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", line):
-		print("POSSIBLE IP ADDRESS: " + line)
+
+	lowercase = line.lower()
+
+	if re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", lowercase):
+		printColor("POSSIBLE IP ADDRESS: ", line)
 	
-	if re.search(r"[Cc]:\\", line):
-		print("POSSIBLE WINDOWS PATH: " + line)
+	if re.search(r"[Cc]:\\", lowercase):
+		printColor("POSSIBLE WINDOWS PATH: ", line)
 
-	if re.search(r"(https)|(http)", line):
-		print("POSSIBLE URI: " + line)
+	if re.search(r"(https)|(http)", lowercase):
+		printColor("POSSIBLE URI: ", line)
 	
-	if "user-agent" in line.lower() or "user agent" in line.lower():
-		print("POSSIBLE USER-AGENT: " + line)
+	if "user-agent" in lowercase or "user agent" in lowercase:
+		printColor("POSSIBLE USER-AGENT: ", line)
 
-	if re.search(r"(HTTP)|(GET)|(PTTH)|(TEG)", line):
-		print("POSSIBLE HTTP HEADER: " + line)
+	if re.search(r"(HTTP)|(GET)|(PTTH)|(TEG)", line) and len(line) > 6:
+		printColor("POSSIBLE HTTP HEADER: ", line)
 
-	if "connection" in line.lower() or "socket" in line.lower() or "port" in line.lower():
-		print("POSSIBLE NETWORK TERMINOLOGY: " + line)
+	if any(netterm in lowercase for netterm in network_terms) and len(line) > 6:
+		printColor("POSSIBLE NETWORK TERMINOLOGY: ", line)
 
-	if "download" in line.lower() or "upload" in line.lower():
-		print("POSSIBLE FILE TRANSFER: " + line)
+	if "download" in lowercase or "upload" in lowercase:
+		printColor("POSSIBLE FILE TRANSFER: ", line)
 
-	if "bitcoin" in line.lower() or "ethereum" in line.lower() or "monero" in line.lower():
-		print("POSSIBLE CRYPTOCURRENCY REFERENCE: " + line)
+	if any(crypto in lowercase for crypto in cryptocurrencies):
+		printColor("POSSIBLE CRYPTOCURRENCY REFERENCE: ", line)
 
-	if "password" in line.lower() or "passwd" in line.lower() or "shadow" in line.lower() or "pass" in line.lower():
-		print("POSSIBLE PASSWORD ACTIVITY: " + line)
+	if "password" in lowercase or "passwd" in lowercase or "shadow" in lowercase or "pass" in lowercase:
+		printColor("POSSIBLE PASSWORD ACTIVITY: ", line)
+	
+	if any(ext in lowercase for ext in file_extensions):
+		printColor("POSSIBLE FILE EXTENSION: ", line)
 
